c61d4fa4f5b69dea1648f2439fff25afa2e0b55e
Tobias Laurin <52038911+sprunq@users.noreply.github.com>
2021-02-25 23:08:03 +0100

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -5,22 +5,33 @@ from discord.utils import get
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.has_UoS_role = []
 
     @commands.command(aliases=['selfmute'],
                       brief="| Mute yourself.",
                       help="Mutes the user.")
     async def mute(self, ctx):
         member = ctx.author
-        role = get(member.guild.roles, name="Snooze")
-        await member.add_roles(role)
+        mute_role = get(member.guild.roles, name="Snooze")
+        member_roles = member.roles
+        for role in member_roles:
+            if role.name == "UoS Coat of Scrobbles":
+                self.has_UoS_role.append(member.id)
+                uos_role = get(member.guild.roles, name="UoS Coat of Scrobbles")
+                await member.remove_roles(uos_role)
+        await member.add_roles(mute_role)
 
     @commands.command(aliases=[],
                       brief="| Unmute yourself.",
                       help="Unmutes the user.")
     async def unmute(self, ctx):
         member = ctx.author
-        role = get(member.guild.roles, name="Snooze")
-        await member.remove_roles(role)
+        mute_role = get(member.guild.roles, name="Snooze")
+        uos_role = get(member.guild.roles, name="UoS Coat of Scrobbles")
+        if member.id in self.has_UoS_role:
+            self.has_UoS_role.remove(member.id)
+            await member.add_roles(uos_role)
+        await member.remove_roles(mute_role)
 
 
 def setup(bot):
